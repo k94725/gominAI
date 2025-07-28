4606ae2e1d73c0a38cefed028394e98f24c45584
k94725 <k94725.dev@gmail.com>
2025-07-28 17:21:32 +0900

    feat: GitHub Pages 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 꼭 필요: static export
+  images: {
+    unoptimized: true, // static export 시 필수
+  },
+  basePath: "/gominAI", // GitHub Pages 하위 경로 대응
+  assetPrefix: "/gominAI/", // static 파일 prefix
 };
 
 export default nextConfig;
